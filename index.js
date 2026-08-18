554527dde8eb9933ece994ac3fce52175e1dbde3
Sunn1709 <030340240147@st.buh.edu.vn>
2026-08-18 20:27:43 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Đăng nhập thành công: ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
